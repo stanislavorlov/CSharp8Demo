25fe6a1f6244cb9f3d6b36cc7aa612843d0b2fca
Stanislav Orlov <stanislav.orlov@external.cnhind.com>
2020-04-15 15:03:07 +0300

    Add project files.

---
+++ CSharp8/AttributesOrProperties.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSharp8
+{
+    internal class AttributesOrProperties
+    {
+        class MyAttribute : Attribute { }
+
+        public Version CSharp7
+        {
+            [My]
+            get;
+        } = new Version("8.0");
+    }
+}
---
+++ CSharp8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+using CSharp8;
+
+namespace CSharp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AttributesOrProperties myClass = new AttributesOrProperties();
+            Console.WriteLine(myClass);
+
+            Type myType = typeof(AttributesOrProperties);
+            PropertyInfo property = myType.GetProperty("CSharp7");
+        }
+    }
+}
